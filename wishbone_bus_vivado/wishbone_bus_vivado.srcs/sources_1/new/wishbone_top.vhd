f6de575a5911eb2c8e7e259a785238380acee7b4
andrew ang <andrewang321@gmail.com>
2018-09-05 23:04:37 +1000

    added read and write handlers

--- wishbone_bus_vivado/wishbone_bus_vivado.srcs/sources_1/new/wishbone_top.vhd
+++ wishbone_bus_vivado/wishbone_bus_vivado.srcs/sources_1/new/wishbone_top.vhd
@@ -11,6 +11,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 
 entity wishbone_interface is
@@ -20,12 +21,12 @@ entity wishbone_interface is
         wb_rst          : in std_logic;
         
         --wishbone bus signals
-        wb_adr_i        : in std_logic_vector(2 downto 0);
+        wb_addr_i        : in std_logic_vector(2 downto 0);
         wb_dat_i        : in std_logic_vector(7 downto 0);
         wb_dat_o        : out std_logic_vector(7 downto 0);
         wb_we_i         : in std_logic;
         wb_sel_i        : in std_logic_vector(3 downto 0);
-        wb_std_i        : in std_logic;
+        wb_stb_i        : in std_logic;
         wb_ack_o        : out std_logic;
         wb_cyc_i        : in std_logic
         
@@ -34,17 +35,47 @@ end wishbone_interface;
 
 architecture Behavioral of wishbone_interface is
 
+    type handshake_state is (handshake_idle,handshake_send_ack,handshake_wait_stb,handshake_done);
+    signal handshake_current_state    : handshake_state := handshake_idle;
+    
+    type memory is array(0 to 1) of std_logic_vector(7 downto 0); -- just the one memory address for now
+    
+    
+    
 begin
     
+    --handshaking protocol
     process(wb_clk,wb_rst)
     begin
         if(rising_edge(wb_clk)) then
-            if(wb_rst = '1') then
-                
+            wb_ack_o <= wb_stb_i; -- acknowledge the bus transaction request
+        end if;
+    end process;
+    
+    -- writes to the slave
+    process(wb_clk,wb_rst)
+    begin
+        if(rising_edge(wb_clk)) then
+            if wb_stb_i and wb_we_i then -- we have a valid write request
+                -- do some logic here to handle data to be written
+
             end if;
         end if;
     end process;
-
-
+    
+    -- reads from the slave
+    process(wb_clk,wb_rst)
+    begin
+        if(rising_edge(wb_clk)) then
+            case wb_addr_i is
+                -- do some logic here to handle reading of data given address
+                -- remember to add default
+                when 0 =>
+                default=>
+            end case;
+        end if;
+    end process;
+    
+    
 end Behavioral;
 
